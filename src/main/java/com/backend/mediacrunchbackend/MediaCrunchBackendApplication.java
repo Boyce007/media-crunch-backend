7f31074719ae22842d7b79f4d385ed3ffd38343c
Daniel Boyce <diboyce21students.desu.edu@Daniels-MacBook-Pro-2.local>
2024-04-08 18:44:52 -0400

    first commit

---
+++ src/main/java/com/backend/mediacrunchbackend/MediaCrunchBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.mediacrunchbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaCrunchBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaCrunchBackendApplication.class, args);
+	}
+
+}
